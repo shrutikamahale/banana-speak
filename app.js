6550660e1748321bb607f6e567a7eaf284aee576
shrutikamahale <shrutika.m.14@gmail.com>
2020-12-05 23:21:19 +0530

    banana app done

--- app.js
+++ app.js
@@ -2,7 +2,27 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-function clickHandler() {
-  outputDiv.innerText = "hadhjadhahda " + txtInput.value
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text
 }
-btnTranslate.addEventListener("click",  clickHandler)
+
+
+function clickHandler() {
+    // outputDiv.innerText = "hadhjadhahda " + txtInput.value
+    
+    var inputText = txtInput.value; // user input
+    
+    //calling server for processing.
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+    })
+
+};
+btnTranslate.addEventListener("click", clickHandler)
